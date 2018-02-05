e62bfe1455984092ec3d2a062f66a410e6766db3
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2018-02-05 13:00:16 -0800

    Add routes

---
+++ routes/wiki.js
@@ -0,0 +1,12 @@
+// const express = require('express')
+// const router = express.Router()
+
+// router.get('/', (request, response) =>{
+//   response.send("Wiki home page")
+// })
+
+// router.get('/about', (request, response) => {
+//   response.send('About this wiki')
+// })
+
+// module.exports = router
